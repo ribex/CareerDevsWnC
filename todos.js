5cf13eac321d06785b6b48ef87809aeca99b3ed2
Rebecca Wagaman <ribexy@gmail.com>
2017-07-11 00:19:57 +0000

    displayTodos should show completed status

--- todos.js
+++ todos.js
@@ -5,9 +5,13 @@ var todoList = {
     if (this.todos.length === 0) {
       console.log("Your todo list is empty!");
     } else {
+      console.log('My todos:');
       for (var i = 0; i < this.todos.length; i++) {
-        console.log('My todos:');
-        console.log(this.todos[i].todoText);
+        if (this.todos[i].completed === true) {
+          console.log("(x)", this.todos[i].todoText);
+        } else {
+          console.log("( )", this.todos[i].todoText);
+        }
       }      
     }
   },
@@ -33,6 +37,6 @@ var todoList = {
   }
 };
 
-todoList.addTodo("This is the final third todo");
-todoList.changeTodo(2, "The final second todo");
-todoList.deleteTodo(1);
+// todoList.addTodo("This is the final third todo");
+// todoList.changeTodo(2, "The final second todo");
+// todoList.deleteTodo(1);
