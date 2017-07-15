c29ae49d84da7e45143b0fa55505494aa732b3e6
Rebecca Wagaman <ribexy@gmail.com>
2017-07-15 17:40:16 +0000

    Display li element for each item in the todo list

--- todos.js
+++ todos.js
@@ -88,3 +88,16 @@ var handlers = {
     todoList.toggleAll();
   }
 };
+
+var view = {
+  displayTodos: function() {
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
+    
+    for (var i = 0; i < todoList.todos.length; i++) {
+      var todosLi = document.createElement('li');
+      todosUl.appendChild(todosLi);
+      
+    }
+  }
+}
