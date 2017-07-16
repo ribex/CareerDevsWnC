72b0dcd3c3bb91e4296e4518e6d33777d4a27900
Rebecca Wagaman <ribexy@gmail.com>
2017-07-16 18:24:48 +0000

    Each delete button has access to li position in array

--- todos.js
+++ todos.js
@@ -86,7 +86,7 @@ var view = {
       } else {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
-      
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
